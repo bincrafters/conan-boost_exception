2f09b19de43309e5e52a8cf9197526890ef42ae7
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:04 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostExceptionConan(ConanFile):
     name = "boost_exception"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_exception"
-    
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["exception"]
     is_header_only = False
 
